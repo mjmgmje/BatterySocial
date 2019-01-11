488b7f31507e50c8592a8e5a750428dd638bdfc3
Manuel J. Bellido-Lama <mjbellido@ayesa.com>
2019-01-11 13:50:40 +0100

    tab 1, 2 , 3 base design added
    tab 1 with notices, tab 2 with 4 options clickable and tab 3 with user info

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  notices = [
+    {
+      id: 1,
+      thumb:
+        'https://s3.amazonaws.com/ionic-marketplace/ionic-3-start-theme/screenshot_1.png',
+      name: 'noticia',
+      description: 'descripcion de la noticia',
+      time: '2d',
+    }
+  ];
+
+  constructor() {}
+
+  viewDetail(id: number) {}
+}
